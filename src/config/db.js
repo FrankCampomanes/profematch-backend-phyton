c138184adb1839d771d06e28d9a06ed0ee047ee6
Franksua <U23229322@gmail.com>
2026-07-09 23:29:58 -0500

    Preparar BD para la nube

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config(); // Para leer variables de entorno en migraciones
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root', // XAMPP default
-    password: '', // XAMPP default is empty
-    database: 'profematch_db',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root', // XAMPP default
+    password: process.env.DB_PASSWORD || '', // XAMPP default is empty
+    database: process.env.DB_NAME || 'profematch_db',
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
